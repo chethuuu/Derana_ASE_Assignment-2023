fc6b907fa65cb63c32cebb70edfd93a55081b4f8
chethuuu <chethanaprasadi2000@gmail.com>
2023-02-16 22:58:55 +0530

    Create Server and DB Connection

---
+++ backend/Server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+//Connect MongoDB
+mongoose.connect(
+    process.env.DB_URL, {
+    //type warnings
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Mongo DB Connected Successfully");
+}).catch((err) => console.log("DB Connection Failed", err));
+
+//Port    
+const PORT = 5000
+app.listen(PORT, () => console.log(`Backend Server is running on PORT ${PORT}`))
